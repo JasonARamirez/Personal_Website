896f9147abc8168c2d7c7bf1c444dda9174e7ea8
Jason Ramirez <jp51371@mac-air-11.cs.iastate.edu>
2017-03-06 19:26:52 -0600

    Filling in the website

--- routing/start.js
+++ routing/start.js
@@ -5,5 +5,9 @@ router.get('/', function(req, res){
     res.sendFile(__dirname + '/public_website/html/index.html');
 });
 
+router.get('/home_view.html', function(req, res){
+  res.sendFile(__dirname + '/public_website/html/home_view.html');
+});
+
 
 module.exports = router;
