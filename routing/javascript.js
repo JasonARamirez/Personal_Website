d11c4bdd3f654a62c007d0c58890cc7dc84e4a03
jp51371 <jp51371@iastate.edu>
2017-03-07 00:31:37 -0600

    Finished Home Page

--- routing/javascript.js
+++ routing/javascript.js
@@ -5,5 +5,13 @@ router.get('/app.js', function(req, res){
     res.sendFile(__dirname + '/public_website/javascript/app.js');
 });
 
+router.get('/home_controller.js', function(req, res){
+  res.sendFile(__dirname + '/public_website/javascript/home_controller.js');
+})
+
+router.get('/main.js', function(req, res){
+  res.sendFile(__dirname + '/public_website/javascript/main.js');
+});
+
 
 module.exports = router;
