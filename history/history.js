8347b4ae5ef70bd8cd82e01379680accd501cd9f
Jason Ramirez <jp51371@mac-air-11.cs.iastate.edu>
2017-04-25 12:17:44 -0500

    h

--- history/history.js
+++ history/history.js
@@ -1,4 +1,9 @@
+const request = require('request');
+const ip_api = 'http://api.db-ip.com/v2/'
 module.exports = function(req, intent){
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(ip);
+  var url = ip_api + process.env.ip_key + '/' + ip;
+  request(url, function(error, res, body) {
+    console.log(JSON.parse(body));
+  });
 }
